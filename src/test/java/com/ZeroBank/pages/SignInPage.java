92f43b281221001c7eaaa861bef74ca35f13f46b
Harun1233 <av.harunonat@gmail.com>
2022-01-22 21:10:49 +0000

    WebElements have been added

--- src/test/java/com/ZeroBank/pages/SignInPage.java
+++ src/test/java/com/ZeroBank/pages/SignInPage.java
@@ -1,6 +1,25 @@
 package com.ZeroBank.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class SignInPage extends BasePage{
+    @FindBy(css = "#user_login")
+    public WebElement userNameInput;
+    @FindBy(css = "#user_password")
+    public WebElement passwordInput;
+    @FindBy(xpath = "//input[@type='submit']")
+    public WebElement signInButton;
+    @FindBy(xpath = "//*[@class='alert alert-error']")
+    public WebElement warningMessage;
+
+
+
+
+
+
+
+
     @Override
     public String getElementText() {
         return null;

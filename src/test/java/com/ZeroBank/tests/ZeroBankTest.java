3fc2a58b00eb265097e2f1f929c72f6c3a42f6fb
Harun1233 <av.harunonat@gmail.com>
2022-01-22 21:27:43 +0000

    Typo has been corrected on properties

--- src/test/java/com/ZeroBank/tests/ZeroBankTest.java
+++ src/test/java/com/ZeroBank/tests/ZeroBankTest.java
@@ -1,4 +1,19 @@
 package com.ZeroBank.tests;
 
-public class ZeroBankTest {
+import com.ZeroBank.pages.SignInPage;
+import com.ZeroBank.tests.utilities.ConfigurationReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZeroBankTest extends TestBase{
+    SignInPage sigIn=new SignInPage();
+
+    @Test
+    public void negativeTestForLogIn(){
+        String actualWarningMessage = sigIn.fillAndSignNegative();
+        String expectedWarningMessage= ConfigurationReader.get("expectedWarningMessage");
+
+        Assert.assertEquals(actualWarningMessage,expectedWarningMessage,"Verify that the warning messages match");
+
+    }
 }

65711d5289346e918338f1fb7ec696eb928e3dc4
Harun1233 <av.harunonat@gmail.com>
2022-01-22 21:06:57 +0000

    HomePage is completed

--- src/test/java/com/ZeroBank/pages/HomePage.java
+++ src/test/java/com/ZeroBank/pages/HomePage.java
@@ -1,14 +1,17 @@
 package com.ZeroBank.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage extends BasePage {
 
+    @FindBy(css = "#signin_button")
+    public WebElement signInButton;
 
 
-
-
-
-
-
+    public void signIn(){
+        signInButton.click();
+    }
 
 
     @Override
